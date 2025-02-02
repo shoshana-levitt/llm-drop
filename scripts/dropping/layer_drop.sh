49905e4fc14ba27d7bcd30a152107494312a8d36
Shoshana Levitt <shoshana.levitt@gmail.com>
2025-02-02 13:40:17 -0500

    commit 2/2/25

--- scripts/dropping/layer_drop.sh
+++ scripts/dropping/layer_drop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-port="21304"
-GPUs="0,1,2,3"
+# port="21304"
+# GPUs="0,1,2,3"
 
 dataset="c4_val"
 prune_data_type="pt"
@@ -14,7 +14,7 @@ target_layer="attn"
 drop_n=8
 
 model_name=mistral-base
-model_name_or_path=mistralai/Mistral-7B-v0.1
+model_name_or_path=mistralai/Mistral-7B-v0.3
 
 folder_name="${model_name}-${prune_method}_${target_layer}-${layer_drop_method}-drop${drop_n}"
 similarity_cache_file="../results_prune/cache/${model_name}-${prune_method}_${target_layer}-${dataset}-${n_calibration_samples}samples.pt"
@@ -24,7 +24,8 @@ echo ${folder_name}
 output_dir=../results_prune/${folder_name}
 prune_model_save_path=${output_dir}/checkpoint
 
-CUDA_VISIBLE_DEVICES=$GPUs accelerate launch --main_process_port $port \
+# CUDA_VISIBLE_DEVICES=$GPUs accelerate launch --main_process_port $port \
+  accelerate launch \
   src/compress.py \
   --stage prune \
   --model_name_or_path ${model_name_or_path} \
@@ -38,13 +39,13 @@ CUDA_VISIBLE_DEVICES=$GPUs accelerate launch --main_process_port $port \
   --cutoff_len ${seq_len} \
   --output_dir ${output_dir} \
   --logging_steps 10 \
-  --bf16 \
   --n_calibration_samples ${n_calibration_samples} \
   --prune_method ${prune_method} \
   --layer_drop_method ${layer_drop_method} \
   --drop_n ${drop_n} \
   --similarity_cache_file ${similarity_cache_file} \
   --prune_model_save_path ${prune_model_save_path}
+    # --bf16 \
 
 
 layer_drop_method="post_dropping"
@@ -64,10 +65,10 @@ python src/compress.py \
   --cutoff_len ${seq_len} \
   --output_dir ${output_dir} \
   --logging_steps 10 \
-  --bf16 \
   --n_calibration_samples ${n_calibration_samples} \
   --prune_method ${prune_method} \
   --layer_drop_method ${layer_drop_method} \
   --drop_n ${drop_n} \
   --similarity_cache_file ${similarity_cache_file} \
   --prune_model_save_path ${prune_model_save_path}
+    # --bf16 \
--- src/llmtuner/compression/prune/__init__.py
+++ src/llmtuner/compression/prune/__init__.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 from .workflow import run_prune
 
-
 __all__ = ["run_prune"]
-=======
-from .workflow import run_prune
-
-
-__all__ = ["run_prune"]
->>>>>>> bcf4a4ef69836bf21e627e22a6de3369782bb159
