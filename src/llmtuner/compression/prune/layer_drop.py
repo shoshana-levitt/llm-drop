d1a5914750cf49b67092b76d4bfe19dfef152d7b
Shoshana Levitt <shoshana.levitt@gmail.com>
2025-03-23 19:08:17 -0400

    add print line

--- src/llmtuner/compression/prune/layer_drop.py
+++ src/llmtuner/compression/prune/layer_drop.py
@@ -126,6 +126,7 @@ def get_layer_similarities(model, dataloader: DataLoader, accelerator: Accelerat
                     np_input_hidden_states = input_hidden_states.cpu().detach().numpy
                     np_output_hidden_states = output_hidden_states.cpu().detach().numpy
                     mut_info = ee.mi(np_input_hidden_states, np_output_hidden_states)
+                    accelerator.print(f'layer {i} similarity: {mut_info.item()}')
                     similarities[i] = mut_info
 
             else:
