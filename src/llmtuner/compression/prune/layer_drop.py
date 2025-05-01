5e8b133840719da163e307e345c24149681472d0
Shoshana Levitt <shoshana.levitt@gmail.com>
2025-05-01 16:25:42 -0400

    remove cpu detach

--- src/llmtuner/compression/prune/layer_drop.py
+++ src/llmtuner/compression/prune/layer_drop.py
@@ -123,8 +123,10 @@ def get_layer_similarities(model, dataloader: DataLoader, accelerator: Accelerat
                     similarities[i] = cos_sim
 
                 elif sim_type == 'mut_info':
-                    np_input_hidden_states = input_hidden_states.cpu().detach().numpy
-                    np_output_hidden_states = output_hidden_states.cpu().detach().numpy
+                    # np_input_hidden_states = input_hidden_states.cpu().detach().numpy
+                    # np_output_hidden_states = output_hidden_states.cpu().detach().numpy
+                    np_input_hidden_states = input_hidden_states.numpy()
+                    np_output_hidden_states = output_hidden_states.numpy()
                     mut_info = ee.mi(np_input_hidden_states, np_output_hidden_states)
                     accelerator.print(f'layer {i} similarity: {mut_info.item()}')
                     similarities[i] = mut_info
