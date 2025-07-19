0f543b0449414f5ebe9140bb07cc3b40e004194c
Shoshana Levitt <shoshana.levitt@gmail.com>
2025-07-19 18:23:47 -0400

    add llama

--- scripts/dropping/layer_drop_mi.sh
+++ scripts/dropping/layer_drop_mi.sh
@@ -13,8 +13,11 @@ layer_drop_method="discrete"
 target_layer="attn"
 drop_n=8
 
-model_name=mistral-base
-model_name_or_path=shoshana-levitt/Mistral-7B-v0.1 # change path to shoshana-levitt/Mistral-7B-v0.1
+# model_name=mistral-base
+# model_name_or_path=shoshana-levitt/Mistral-7B-v0.1
+
+model_name=llama-base
+model_name_or_path=shoshana-levitt/shoshana-levitt/Llama-3.1-8B-Instruct
 
 # MUTUAL INFO
 
