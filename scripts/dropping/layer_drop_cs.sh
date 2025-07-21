77a39d08e45c5582f3ff54bc46df2b1a1fac3947
Shoshana Levitt <shoshana.levitt@gmail.com>
2025-07-21 11:33:32 -0400

    change model name and similarity cache file name

--- scripts/dropping/layer_drop_cs.sh
+++ scripts/dropping/layer_drop_cs.sh
@@ -17,7 +17,7 @@ drop_n=8
 # model_name_or_path=shoshana-levitt/Mistral-7B-v0.1
 
 model_name=llama-base
-model_name_or_path=shoshana-levitt/Llama-3.1-8B-Instruct
+model_name_or_path=shoshana-levitt/Llama-2-7b-hf
 
 # COSINE SIMILARITY
 
@@ -27,7 +27,67 @@ folder_name="${model_name}-${prune_method}_${target_layer}-${layer_drop_method}-
 echo ${folder_name}
 output_dir=../results_prune_${sim_type}/${folder_name}
 prune_model_save_path=${output_dir}/checkpoint
-similarity_cache_file="../results_prune/cache/${model_name}-${prune_method}_${target_layer}-${dataset}-${n_calibration_samples}samples.pt"
+similarity_cache_file="../results_prune/cache/${model_name}-${prune_method}_${sim_type}_${target_layer}-${dataset}-${n_calibration_samples}samples.pt"
+
+CUDA_VISIBLE_DEVICES=$GPUs accelerate launch --main_process_port $port \
+  src/compress.py \
+  --stage prune \
+  --model_name_or_path ${model_name_or_path} \
+  --dataset ${dataset} \
+  --dataset_dir ./src/llmtuner/data \
+  --split "train" \
+  --layer_drop_norm True \
+  --sim_type ${sim_type} \
+  --target_layer ${target_layer} \
+  --only_update_config True \
+  --prune_data_type ${prune_data_type} \
+  --cutoff_len ${seq_len} \
+  --output_dir ${output_dir} \
+  --logging_steps 10 \
+  --bf16 \
+  --n_calibration_samples ${n_calibration_samples} \
+  --prune_method ${prune_method} \
+  --layer_drop_method ${layer_drop_method} \
+  --drop_n ${drop_n} \
+  --similarity_cache_file ${similarity_cache_file} \
+  --prune_model_save_path ${prune_model_save_path}
+
+
+layer_drop_method="post_dropping"
+# set only_update_config to True to save the disk memory
+only_update_config=False
+
+python src/compress.py \
+  --stage prune \
+  --model_name_or_path ${model_name_or_path} \
+  --dataset ${dataset} \
+  --dataset_dir ./src/llmtuner/data \
+  --split "train" \
+  --only_update_config $only_update_config \
+  --layer_drop_norm True \
+  --target_layer ${target_layer} \
+  --sim_type ${sim_type} \
+  --prune_data_type ${prune_data_type} \
+  --cutoff_len ${seq_len} \
+  --output_dir ${output_dir} \
+  --logging_steps 10 \
+  --bf16 \
+  --n_calibration_samples ${n_calibration_samples} \
+  --prune_method ${prune_method} \
+  --layer_drop_method ${layer_drop_method} \
+  --drop_n ${drop_n} \
+  --similarity_cache_file ${similarity_cache_file} \
+  --prune_model_save_path ${prune_model_save_path}
+
+  # MUTUAL INFORMATION
+
+sim_type="mut_info"
+
+folder_name="${model_name}-${prune_method}_${target_layer}-${layer_drop_method}-drop${drop_n}"
+echo ${folder_name}
+output_dir=../results_prune_${sim_type}/${folder_name}
+prune_model_save_path=${output_dir}/checkpoint
+similarity_cache_file="../results_prune/cache/${model_name}-${prune_method}_${sim_type}_${target_layer}-${dataset}-${n_calibration_samples}samples.pt"
 
 CUDA_VISIBLE_DEVICES=$GPUs accelerate launch --main_process_port $port \
   src/compress.py \
